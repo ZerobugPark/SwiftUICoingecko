1d43f12e8662cb2f1214f8d0f08e395a4d8f5f1b
young <zerobugpark@gmail.com>
2025-04-19 13:11:05 +0900

    Initial Commit

---
+++ SwiftUICoingecko/SwiftUICoingeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoingeckoApp.swift
+//  SwiftUICoingecko
+//
+//  Created by youngkyun park on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoingeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
